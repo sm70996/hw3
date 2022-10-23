e73d090e2d3a353fb4b6e5586704f076e5e42336
SALVADOR MONTOYA <sm70996@usc.edu>
2022-10-23 00:22:21 +0000

    All done

---
+++ logicsim/event.h
@@ -0,0 +1,23 @@
+#ifndef EVENT_H
+#define EVENT_H
+#include "wire.h"
+
+struct Event
+{
+    uint64_t time;
+    Wire* wire;
+    char state;
+};
+
+typedef struct EventLess {
+        //write the operator() required to make this a functor that compares Events by time
+    bool operator() (Event* a, Event* b)
+    {
+        if(a->time < b->time){
+            return true; 
+        }
+        return false; 
+    }
+} EventLess;
+	
+#endif
