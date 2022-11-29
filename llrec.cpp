6d5c851454333cc0549159c698e2e368839b6bcd
SALVADOR MONTOYA <sm70996@usc.edu>
2022-11-29 23:47:30 +0000

    Fixed code

--- llrec.cpp
+++ llrec.cpp
@@ -9,15 +9,16 @@ void llpivot(Node *&head, Node *&smaller, Node *&larger, int pivot){
 		larger = nullptr; 
 		return; 
 	}
-	else{
-		if(head->val <= pivot){
-			smaller = head;
-			llpivot(head->next, smaller->next, larger, pivot);
-		}
-		else if(head->val > pivot){
-			larger = head; 
-			llpivot(head->next, smaller, larger->next, pivot);
-		}
+	Node* temp = head->next; 
+	if(head->val <= pivot){
+		smaller = head;
+    head = nullptr; 
+		llpivot(temp, smaller->next, larger, pivot);
 	}
+	else{
+		larger = head; 
+    head = nullptr; 
+		llpivot(temp, smaller, larger->next, pivot);
+	}	
 }
 
